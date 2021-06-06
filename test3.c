337b46038e30c1ade91e5ce43e59b51b94d0737e
ishui <godeyer@163.com>
2021-06-06 10:32:05 +0800

    add file test3.c

---
+++ test3.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc,char *argv[]){
+    
+    ;
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+    }
+    
+
+
+    return 0;
+}
